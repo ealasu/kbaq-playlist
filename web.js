f7581e81435ffe2f471ce6ac2bd76211b3c26db6
emi <emi@ealasu-mbp15.local>
2012-06-06 23:27:56 -0700

    redis configs

--- web.js
+++ web.js
@@ -1,22 +1,29 @@
 var express = require('express');
 var request = require('request');
-var redis = require('redis');
 
-var client = redis.createClient();
+
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  var redis = require("redis").createClient();
+}
+
 var app = express.createServer(express.logger());
 
 var default_url = 'http://kbaq.org/music/playlists/text?06022012_playlist.txt';
 
 
 var getCached = function(url, callback) {
-  client.get(url, function(err, reply) {
-    if (!reply) {
+  redis.get(url, function(err, reply) {
+    if (reply) {
+      callback(reply);
+    } else {
       request(url, function(req_error, response, body) {
-        client.set(url, body);
+        redis.set(url, body);
         callback(reply);
       });
-    } else {
-      callback(reply);
     }
   });
 };
@@ -26,11 +33,6 @@ app.get('/', function(req, resp) {
   getCached(default_url, function(body) {
     resp.send(body);
   });
-
-
-
-
-
 });
 
 var port = process.env.PORT || 5000;
