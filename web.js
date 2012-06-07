6b275101f3e45fde89d3009eaf94f6f82f5b0813
emi <emi@ealasu-mbp15.local>
2012-06-06 22:37:32 -0700

    do more

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var request = require('request');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, resp) {
+  request('http://kbaq.org/music/playlists/text?06022012_playlist.txt', function(error, response, body) {
+
+
+    resp.send(body);
+  });
 });
 
 var port = process.env.PORT || 5000;
