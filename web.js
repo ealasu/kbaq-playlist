acddd012be8ea45d27851e7a99216a2975e1258e
emi <emi@ealasu-mbp15.local>
2012-06-06 23:21:58 -0700

    redis init

--- web.js
+++ web.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var request = require('request');
+var redis = require('redis');
 
+var client = redis.createClient();
 var app = express.createServer(express.logger());
 
+var default_url = 'http://kbaq.org/music/playlists/text?06022012_playlist.txt';
+
+
+var getCached = function(url, callback) {
+  client.get(url, function(err, reply) {
+    if (!reply) {
+      request(url, function(req_error, response, body) {
+        client.set(url, body);
+        callback(reply);
+      });
+    } else {
+      callback(reply);
+    }
+  });
+};
+
+
 app.get('/', function(req, resp) {
-  request('http://kbaq.org/music/playlists/text?06022012_playlist.txt', function(error, response, body) {
-
-
+  getCached(default_url, function(body) {
     resp.send(body);
   });
+
+
+
+
+
 });
 
 var port = process.env.PORT || 5000;
