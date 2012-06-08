70e1301cb12acdaa6578ec2f5fd210626d8e69b4
emi <emi@ealasu-mbp15.local>
2012-06-07 21:16:20 -0700

    some improvements

--- web.js
+++ web.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var request = require('request');
+var jsdom = require('jsdom');
+var _ = require('underscore');
+require('datejs');
 
 
 if (process.env.REDISTOGO_URL) {
@@ -10,28 +13,100 @@ if (process.env.REDISTOGO_URL) {
   var redis = require("redis").createClient();
 }
 
-var app = express.createServer(express.logger());
+var default_date = Date.today();
 
-var default_url = 'http://kbaq.org/music/playlists/text?06022012_playlist.txt';
+var partitionBy = function(obj, val) {
+  var last = [];
+  var result = [last];
+  var lastVal = null;
+  _.each(obj, function(value, index) {
+    var currentVal = val(value);
+    if (currentVal !== lastVal && index > 0) {
+      last = [];
+      result.push(last);
+    }
+    lastVal = currentVal;
+    last.push(value);
+  });
+  return result;
+};
 
+var parsePlaylist = function(selector, callback) {
+  var text = selector('p').text();
+  var lines = text.split('\n');
+  var playlistDate = new Date(/Playlist for (.+?)\s*$/g.exec(selector('h4').first().text())[1]);
+  
+  var result = lines;
+  result = _.filter(result, function(val){ return !val.match(/\s*\d+\s*\|\s*\d+\s*/g); });
+  result = partitionBy(result, function(val){ return val.match(/^[\s_]*$/); });
+  result = _.filter(result, function(val){ return !_.any(val, function(v2){return v2.match(/^[\s_]*$/);}); });
+  result = _.filter(result, function(val){ return val[0].match(/^\s*\d+:\d+.*$/); });
+  result = _.map(result, function(val){ 
+    return _.map(val, function(val2){ 
+      return val2.trim(); }); });
 
-var getCached = function(url, callback) {
-  redis.get(url, function(err, reply) {
+  result = _.map(result, function(val) {
+    var first = val[0].match(/^\s*(\d+:\d+\s*[AP]M)\s*(.+$)/i);
+    if (first) {
+      return {
+      'time': Date.parse(playlistDate.toString('M/d/yyyy') + ' ' + first[1]),
+      'name': first[2],
+      'artists': _.initial(_.rest(val)),
+      'album': _.last(val)
+      };
+    }
+  });
+
+  return result;
+}
+
+var getPlaylistUrl = function(playlistDate) {
+  return 'http://kbaq.org/music/playlists/text?' + playlistDate.toString('MMddyyyy') + '_playlist.txt';
+};
+
+var getPlaylist = function(playlistDate, callback) {
+  jsdom.env(getPlaylistUrl(playlistDate), [
+    'http://code.jquery.com/jquery-1.5.min.js'
+  ],
+  function(errors, window) {
+    callback(parsePlaylist(window.$));
+    window.close();
+  });
+};
+
+var getCached = function(playlistDate, callback) {
+  redis.get(playlistDate, function(err, reply) {
     if (reply) {
-      callback(reply);
+      callback(JSON.parse(reply));
     } else {
-      request(url, function(req_error, response, body) {
-        redis.set(url, body);
-        callback(body);
+      getPlaylist(playlistDate, function(playlist) {
+        redis.set(playlistDate, JSON.stringify(playlist));
+        callback(playlist);
       });
     }
   });
 };
 
 
-app.get('/', function(req, resp) {
-  getCached(default_url, function(body) {
-    resp.send(body);
+var app = express.createServer(express.logger());
+
+app.configure(function() {
+  app.use(express.static(__dirname + '/public'));
+  app.set('view engine', 'jade');
+});
+
+app.get('/', function(req, res) {
+  getCached(Date.today(), function(playlist) {
+    res.render('index', {
+      'title': "Today's playlist", 
+      'playlist': playlist
+    });
+  });
+});
+
+app.get('/playlist/:date', function(req, res) {
+  getCached(Date.parse(req.params.date), function(body) {
+    res.send(body);
   });
 });
 
